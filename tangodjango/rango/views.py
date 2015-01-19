4471e5c19166c7d8e3ee908ae14f805acd792ee2
unknown <2072704N@BO706-30.ad.dcs.gla.ac.uk>
2015-01-19 15:08:16 +0000

    Chapter 5 - Completed

--- tangodjango/rango/views.py
+++ tangodjango/rango/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<HTML> Rango says hey there world! <a href='http://127.0.0.1:8000/rango/about/'> about page </a></HTML>")
+    context_dict = {'boldmessage': 'I am bold font from the context'}
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("<HTML> Rango says here is the about page.----- This tutorial has been put together by Callum Nixon, 2072704N <a href='http://127.0.0.1:8000/rango/'> index page </a></HTML>")
+    return render(request,'rango/about.html')
--- tangodjango/tangodjango/settings.py
+++ tangodjango/tangodjango/settings.py
@@ -12,7 +12,12 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
+TEMPLATE_DIRS = [
+    #H:/tangodjango/templates/
+    TEMPLATE_PATH,
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -81,4 +86,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (STATIC_PATH,)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #absolute path to media directory
