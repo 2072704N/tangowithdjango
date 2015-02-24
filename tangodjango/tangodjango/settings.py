307c85870023f06d24f43465d18bfd9a34bf6637
unknown <2072704N@BO706-22.ad.dcs.gla.ac.uk>
2015-02-24 10:53:36 +0000

    Chapter 12 completed

--- tangodjango/tangodjango/settings.py
+++ tangodjango/tangodjango/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -101,3 +102,11 @@ STATICFILES_DIRS = (STATIC_PATH,)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #absolute path to media directory
+
+# registration stuff
+
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/rango/'
+LOGIN_URL = '/accounts/login/'
--- tangodjango/tangodjango/urls.py
+++ tangodjango/tangodjango/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from registration.backends.simple.views import RegistrationView
+from django.contrib.auth import views 
 
+class MyRegistrationView(RegistrationView):
+    def get_success_url(selfself,request,user):
+        return '/rango/'
+    
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tangodjango.views.home', name='home'),
@@ -9,6 +15,16 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(),name = 'registration_register'),
+    (r'^accounts/', include('registration.backends.simple.urls')),
+    #override registration urls
+    url(r'^password/change/$',
+                views.password_change,
+                name='password_change'),
+    #bit hacky but redirects user to rango once password change is done
+    url(r'^rango/$',
+                views.password_change_done,
+                name='password_change_done'),
 )
 
 if settings.DEBUG:
